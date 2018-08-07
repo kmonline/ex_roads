6a6b3e8e964a97493bba159737db175be4d40e07
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2018-08-07 19:34:56 -0300

    Introduce ExGuard for running tests

--- mix.exs
+++ mix.exs
@@ -34,7 +34,8 @@ defmodule GoogleRoads.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      {:ex_doc, "~> 0.19.1", only: :dev}
+      {:ex_doc, "~> 0.19.1", only: :dev},
+      {:ex_guard, "~> 1.3", only: :dev}
     ]
   end
 
