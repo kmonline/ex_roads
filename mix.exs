95d19a064c1a1877ba236b422d26b52b3c740a70
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2018-08-08 11:11:25 -0300

    Add httpoison and jason hex to mix

--- mix.exs
+++ mix.exs
@@ -34,6 +34,8 @@ defmodule GoogleRoads.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:httpoison, "~> 1.2.0"},
+      {:jason, "~> 1.1.1"},
       {:ex_doc, "~> 0.19.1", only: :dev},
       {:ex_guard, "~> 1.3", only: :dev}
     ]
