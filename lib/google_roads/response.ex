3cbcc60bed71df62f95ab3a8b0f8af816aebdf40
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2018-08-09 12:25:30 -0300

    Implement pattern matching for Response

--- lib/google_roads/response.ex
+++ lib/google_roads/response.ex
@@ -7,11 +7,10 @@ defmodule GoogleRoads.Response do
 
   @type error :: HTTPoison.Error.t | status()
 
-  def wrap({:error, error}), do: {:error, error}
+  def wrap({:ok, %{body: %{"error" => %{"status" => status, "message" => error_message}}}}), do: {:error, status, error_message}
   def wrap({:ok, %{body: body} = response}) when is_binary(body) do
     wrap({:ok, %{response | body: Jason.decode!(body)}})
   end
-  def wrap({:ok, %{body: %{"status" => "OK"} = body}}), do: {:ok, body}
-  def wrap({:ok, %{body: %{"status" => status, "error_message" => error_message}}}), do: {:error, status, error_message}
-  def wrap({:ok, %{body: %{"status" => status}}}), do: {:error, status}
+  def wrap({:ok, %{body: body}}), do: {:ok, body}
+  def wrap({:error, %{reason: reason}}), do: {:error, reason}
 end
